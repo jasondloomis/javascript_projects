fef66347e1d7c28170ccb69a28bc81253b507933
Jason Loomis <jasondloomis@icloud.com>
2020-08-16 21:59:00 -0700

    fix alert setting

--- Basic_JS_projects/js/basic_js_1.js
+++ Basic_JS_projects/js/basic_js_1.js
@@ -1,10 +1,8 @@
-var greeting = "Hello, World!" + " What is your name?" + "<br>", y = 11; // multiple variables on one line, concatenate with line break
-var x = 5;
-var color = greeting.fontcolor("blue");
+var greeting = "Hello, World!" + " What is your name?" + "<br>", y = 11, x = 5; // multiple variables on one line, concatenate with line break
+var color = greeting.fontcolor("blue"); // change font color to blue
 
 var aString = "This is an alert of the crazy \"broadcast system\"! This message will repeat " + y + " times." + "<br>"; // concatenate with line break
 
 document.write(color);
-// alert(aString);
 document.write(aString);
-document.write(x + y); // add x plus y
+alert(x + y); // add variables
