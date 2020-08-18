d843770678b506e944f414e5ebfbafe146c48775
Jason Loomis <jasondloomis@icloud.com>
2020-08-17 17:31:52 -0700

    add data for js assignment 3

--- Basic_JS_projects/js/basic_js_3.js
+++ Basic_JS_projects/js/basic_js_3.js
@@ -3,3 +3,31 @@ function sumNums(x, y) {
     x -= y;
     document.getElementById("minus").innerHTML = x;
 }
+
+function randomNums() {
+    x = Math.random() * 75;
+    round = Math.round(x)
+    document.getElementById("random").innerHTML = round;
+}
+
+function remainModulus(x, y) {
+    x %= y;
+    document.getElementById("modulus").innerHTML = x;
+}
+
+function multiplyAdd() {
+    x = 10 * 1 + 2;
+    document.getElementById("multiply").innerHTML = x;
+}
+
+function incrementOne() {
+    x = 12
+    x++
+    document.getElementById("increment").innerHTML = x;
+}
+
+function decreaseOne() {
+    x = 12
+    x--
+    document.getElementById("decrease").innerHTML = x;
+}
